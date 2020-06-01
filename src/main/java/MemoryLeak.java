d4da844451ab6abb46cfdb4328e3a6aae633c258
ArchieTramp <64407929+ArchieTramp@users.noreply.github.com>
2020-06-01 23:20:12 +0500

    1

--- src/main/java/MemoryLeak.java
+++ src/main/java/MemoryLeak.java
@@ -1,37 +1,36 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
 
-/** -XX:+UseSerialGC -Xmx10m
- * -XX:MaxPermSize -Xmx32m
- * MaxMetaspaceSize -Xmx32m*/
+/**
+ * -XX:MaxMetaspaceSize=10m
+ */
 
 public class MemoryLeak {
 
     private static final int LOOP = 100_000_000;
-    static final ArrayList lister = new ArrayList(100);
 
     public static void main(String[] args) throws InterruptedException {
-//        Thread thread = new Thread();
-//        thread.start();
-        List<String> list  = new ArrayList<>();
-        Stream stream = list.parallelStream();
-        System.out.println(list);
 
+
+        List<String> list = new ArrayList<>();
 
         Random random = new Random();
         for (int i = 0; i < LOOP; i++) {
             String str = "" + random.nextInt();
             list.add(str);
+
+            Stream stream = list.parallelStream();
+            stream.map(x -> x + "xx");
+            System.out.println(list);
+            Thread thread = new Thread();
+            thread.start();
             if (i % 10 == 0) {
                 Thread.sleep(100);
                 list.remove(0);
             }
-            
+
         }
         System.out.println(list.size());
     }
